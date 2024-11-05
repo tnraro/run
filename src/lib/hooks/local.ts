4f5fb4d1c0be42fdf76dbfd7606a3b80c9530a41
tnraro <deokji53@gmail.com>
2024-11-05 09:12:45 +0900

    add missing removable key

--- src/lib/hooks/local.ts
+++ src/lib/hooks/local.ts
@@ -2,4 +2,5 @@ export function clearLocalStorage(key: string) {
 	localStorage.removeItem(`${key}:code`);
 	localStorage.removeItem(`${key}:code:js`);
 	localStorage.removeItem(`${key}:testcases`);
+	localStorage.removeItem(`${key}:lang`);
 }
